069bae2b59439a8c2e0b6810bf0a04a9ced34910
Thomas Tych <thomas.tych@gmail.com>
2023-03-08 23:13:57 +0100

    [util] add counter

--- util/my_dfu.sh
+++ util/my_dfu.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+i=5
+while [ $i -ge 0 ]; do
+    echo -n "$i "
+    sleep 1
+    i=$(($i - 1))
+done
+echo
+
+set -x
 sudo dfu-programmer atmega32u4 erase
 sleep 1
 sudo dfu-programmer atmega32u4 flash $1
 sleep 1
 sudo dfu-programmer atmega32u4 reset
+set +x
