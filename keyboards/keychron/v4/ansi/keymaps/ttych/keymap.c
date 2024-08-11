976758e75f4142946bbbf8aa5ac697e7f22f87b6
Thomas Tych <thomas.tych@gmail.com>
2024-08-11 11:18:11 +0200

    [keychron/v4:ttych] change MAC/PC layer

--- keyboards/keychron/v4/ansi/keymaps/ttych/keymap.c
+++ keyboards/keychron/v4/ansi/keymaps/ttych/keymap.c
@@ -21,7 +21,8 @@
 // clang-format off
 
 enum layers {
-    MAC,
+    /* MAC, */
+    PC1,
     PC,
     /* _FN1, */
     /* _FN2, */
@@ -115,19 +116,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #define KC_FLXP LGUI(KC_E)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [MAC] = LAYOUT_60_ansi(
+    /* [MAC] = LAYOUT_60_ansi( */
+    /*     QK_GESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, */
+    /*     KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, */
+    /*     MO(_FNC), KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT, */
+    /*     KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT, */
+    /*     KC_LCTL,  KC_LOPT,  KC_LCMD,                            KC_SPC,                             KC_RCMD,  MO(_FN), MO(_FNM), KC_RCTL), */
+
+    [PC1] = LAYOUT_60_ansi(
         QK_GESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
         KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
         MO(_FNC), KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT,
-        KC_LCTL,  KC_LOPT,  KC_LCMD,                            KC_SPC,                             KC_RCMD,  MO(_FN), MO(_FNM), KC_RCTL),
+        KC_LCTL,  KC_LWIN,  KC_LALT,                            KC_SPC,                             KC_RALT,  MO(_FN), MO(_FNM), KC_RCTL),
 
     [PC] = LAYOUT_60_ansi(
         QK_GESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
         KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
         MO(_FNC), KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                            KC_SPC,                             KC_RALT,  MO(_FN), MO(_FNM), KC_RCTL),
+        KC_LCTL,  KC_LWIN,  KC_LALT,                            KC_SPC,                             KC_RALT,  MO(_FN), KC_APP, KC_RCTL),
 
     /* [_FN1] = LAYOUT_60_ansi( */
     /*     KC_GRV,  KC_BRID,  KC_BRIU, KC_NO,   KC_NO,   RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  RGB_MOD, */
@@ -161,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,   KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   KC_DEL,
         KC_F13,   KC_PGUP,  KC_UP,   KC_PGDN, KC_HOME, KC_VOLU, KC_BRIU, KC_WH_U, KC_MS_U, KC_WH_D, KC_PSCR,  KC_SCRL,  KC_PAUS,  KC_INS,
         MO(_FNC), KC_LEFT,  KC_DOWN, KC_RGHT, KC_END,  KC_VOLD, KC_BRID, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1,  KC_BTN2,            RGB_TOG,
-        KC_CAPS,            _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU,  KC_BTN3,            KC_MUTE,
+        _______,            _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU,  KC_BTN3,            KC_MUTE,
         _______,  _______,  _______,                            _______,                            _______,  MO(_FN),  KC_APP,   NK_TOGG),
 
     [_FN] = LAYOUT_60_ansi(
