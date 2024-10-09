f14938b00926c80baf471d845ad95afdacb72c9b
Thomas Tych <thomas.tych@gmail.com>
2024-10-09 12:54:42 +0200

    [keychron/v4:ttych] update M

--- keyboards/keychron/v4/ansi/keymaps/ttych/keymap.c
+++ keyboards/keychron/v4/ansi/keymaps/ttych/keymap.c
@@ -34,10 +34,10 @@ enum layers {
 
 enum custom_keycodes {
     MS_MP = SAFE_RANGE,
-    MS_MSP,
     MS_MPP,
     MS_WWL,
     MS_WWP,
+    MS_WWPL,
     MS_WWC,
     MS_WUL,
     MS_WHL,
@@ -51,12 +51,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   case MS_MP:
     if (record->event.pressed) {
       SEND_STRING(ms_mp);
-    }
-    break;
-
-  case MS_MSP:
-    if (record->event.pressed) {
-      SEND_STRING(ms_msp);
     }
     break;
 
@@ -75,6 +69,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   case MS_WWP:
     if (record->event.pressed) {
       SEND_STRING(ms_wwp);
+    }
+    break;
+
+  case MS_WWPL:
+    if (record->event.pressed) {
+      SEND_STRING(ms_wwpl);
     }
     break;
 
@@ -128,14 +128,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
         MO(_FNC), KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                            KC_SPC,                             KC_RALT,  MO(_FN), MO(_FNM), KC_RCTL),
+        KC_LCTL,  KC_LWIN,  KC_LALT,                            KC_SPC,                             KC_RALT,  MO(_FN),  MO(_FNM), KC_RCTL),
 
     [PC] = LAYOUT_60_ansi(
-        QK_GESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
-        KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
-        MO(_FNC), KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,
-        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                            KC_SPC,                             KC_RALT,  MO(_FN), KC_APP, KC_RCTL),
+        QK_GESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,
+        KC_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,
+        MO(_FNC), KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,           KC_ENT,
+        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,           KC_RSFT,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                            KC_SPC,                             KC_RALT,  MO(_FN),  KC_APP,  KC_RCTL),
 
     /* [_FN1] = LAYOUT_60_ansi( */
     /*     KC_GRV,  KC_BRID,  KC_BRIU, KC_NO,   KC_NO,   RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  RGB_MOD, */
@@ -159,9 +159,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*     _______,  _______,  _______,                            _______,                            _______,  _______,  _______,  _______), */
 
     [_FNM] = LAYOUT_60_ansi(
-        _______,  _______,  _______, _______, _______, _______, _______, _______, _______, _______, MS_MP,    MS_MSP,   MS_MPP,   _______,
+        _______,  _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  MS_MP,    MS_MPP,   _______,
         _______,  _______,  _______, _______, _______, _______, _______, _______, MS_WHL,  MS_WWL,  MS_WUL,   MS_WPP,   _______,  _______,
-        _______,  _______,  _______, _______, _______, _______, _______, _______, MS_WHP,  MS_WWP,  _______,  _______,            _______,
+        _______,  _______,  _______, _______, _______, _______, _______, _______, MS_WHP,  MS_WWP,  MS_WWPL,  _______,            _______,
         _______,            _______, _______, _______, _______, _______, _______, _______, _______, MS_WWC,   _______,            _______,
         _______,  _______,  _______,                            _______,                            _______,  _______,  _______,  _______),
 
@@ -174,9 +174,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FN] = LAYOUT_60_ansi(
         KC_GRV,   KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   KC_DEL,
-        KC_F13,   KC_PGUP,  KC_UP,   KC_PGDN, KC_HOME, _______, KC_BRIU, _______, _______, _______, KC_PSCR,  KC_SCRL,  KC_PAUS,  KC_INS,
-        KC_CAPS,  KC_LEFT,  KC_DOWN, KC_RGHT, KC_END,  _______, KC_BRID, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,  RGB_MOD,            RGB_TOG,
-        _______,            _______, RGB_M_P, RGB_M_SW, RGB_M_R, RGB_M_B, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, RGB_RMOD,           KC_MUTE,
+        KC_F13,   KC_PGUP,  KC_UP,   KC_PGDN, KC_HOME, _______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, KC_PSCR,  KC_SCRL,  KC_PAUS,  KC_INS,
+        KC_CAPS,  KC_LEFT,  KC_DOWN, KC_RGHT, KC_END,  _______, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, _______,  RGB_MOD,            RGB_TOG,
+        _______,            _______, RGB_M_P, RGB_M_SW, RGB_M_R, RGB_M_B, _______, KC_MUTE, KC_VOLD, KC_VOLU, RGB_RMOD,           KC_MUTE,
         _______,  _______,  _______,                            _______,                            _______,  MO(_FN),  KC_APP,   QK_BOOT),
 
 };
